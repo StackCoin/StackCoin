b8e61eee41fd34315479f057d93efadcde359743
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-26 16:57:50 -0330

    db wip

---
+++ sql/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE transactions (
+  guild_id string,
+  guild_name string,
+  author_id string,
+  author_name string,
+  collector_id string,
+  collector_name string,
+  amount integer,
+  time integer
+);
