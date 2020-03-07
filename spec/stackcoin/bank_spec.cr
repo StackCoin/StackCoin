ec1687a0352119440593b34cb31b26b51787c260
Jack Arthur Harrhy <me@jackharrhy.com>
2020-03-07 15:04:52 -0330

    fix specs

--- spec/stackcoin/bank_spec.cr
+++ spec/stackcoin/bank_spec.cr
@@ -8,7 +8,7 @@ daniel_id = 3_u64
 
 def create_empty_test_bank
   db = DB.open "sqlite3://%3Amemory%3A"
-  StackCoin::Database.init db
+  StackCoin::Database.new StackCoin::Config.new, db
   StackCoin::Bank.new db
 end
 
