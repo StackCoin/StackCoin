56ec74747fc0f9b12451706f957370ef7f43124c
Jack Arthur Harrhy <me@jackharrhy.com>
2020-05-15 20:36:14 -0230

    omegalol

--- src/stackcoin/commands/dole.cr
+++ src/stackcoin/commands/dole.cr
@@ -6,54 +6,39 @@ class StackCoin::Bot
       super context
     end
 
-    def invoke(message)
-      # Bet my money on a stupid horse, I lost that
-      # So I ran out to the track to get my cash back
-      # I just gotta leave this place with a big bag
-      # So I found the fuckin' jockey and I grabbed that (pick it up)
-      # Pushed him down to the ground and I punched him in his face (in his face)
-      # Yeah, I stole his phone, that put him in his place (in his place)
-      # Me on the horse, we ran out of the place (the place)
-      # Then we took my Porsche back to my place
-      if message.author.id.to_u64 == 134337759446958081_u64
-        # Stupid horse, I just fell out of the Porsche
-        # Lost the money in my bank account, oh no
-        # Stupid horse, I just fell out of the Porsche
-        # Lost the money in my bank account, oh no
-        # Stupid horse, I just fell out of the Porsche
-        # Lost the money in my bank account, oh no
-        # Stupid horse, I just fell out of the Porsche
-        # Lost the money in my bank account
-        sleep 2.seconds
-        send_msg message, "no sven"
-        # Woo
-        # Pick it up
-        sleep 10.seconds
-        send_msg message, "maybe?"
-        sleep 5.seconds
-        # Stupid horse and a swordfish dancer (pick it up)
-        # Bet my money on a fishnet carousel
-        # Go, go, go, go, go so fast now
-        # Go, go, go, go, go so fast now
-        # Racing horses at the derby
-        # Why am I never getting lucky?
-        # I never have any money
-        # I never win any money
-        send_msg message, "fine..."
-        sleep 2.seconds
+    def loading(message)
+      send_msg message, "loading."
+      sleep 20.seconds
+      send_msg message, "loading.."
+      sleep 20.seconds
+      send_msg message, "loading..."
+      sleep 20.seconds
+    end
+
+    def memes(message)
+      case message.author.id.to_u64
+      when 134337759446958081_u64 # sven
+        loading message
+        send_msg message, "ding!"
+      when 120571255635181568_u64 # z64
+        send_msg message, "m-m-maintainer-san @__@"
+      when 140981598987354112_u64 # bobi
+        loading message
+        send_msg message, "boobi gt doyl"
+      when 72073771468468224_u64 # cheem
+        send_msg message, "DOLE"
+      when 72069821960822784_u64 # duunie
+        send_msg message, "'I need it'"
+      when 163415804761735170_u64 # bigman
+        send_msg message, "frdulnt tranactin dtectd by VraFin™™™™™™™™™™™™"
+      when 636346308403134484_u64 # human lambo
+        send_msg message, "Just send it to <@72073771468468224> now, might as well not wait until he beats you again :)"
+      else
       end
-      # Stupid horse, I just fell out of the Porsche
-      # Lost the money in my bank account, oh no
-      # Stupid horse, I just fell out of the Porsche
-      # Lost the money in my bank account, oh no
-      # Stupid horse, I just fell out of the Porsche (oh shit)
-      # Lost the money in my bank account, oh no (oh no)
-      # Stupid horse, I just fell out of the Porsche (oh no)
-      # Lost the money in my bank account, oh no
-      # Stupid horse, I just fell out of the Porsche (of the Porsche)
-      # Lost the money in my bank account, oh no (oh no)
-      # Stupid horse, I just fell out of the Porsche (of the Porsche)
-      # Lost the money in my bank account
+    end
+
+    def invoke(message)
+      memes message
       send_msg message, @bank.deposit_dole(message.author.id.to_u64).message
     end
   end
