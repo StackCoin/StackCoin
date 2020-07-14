a1c5f39e235f9c2b9bf1362cbbe17dce37fc892a
Jack Arthur Harrhy <me@jackharrhy.com>
2020-07-14 18:34:06 -0230

    debug logging

--- src/stackcoin/const.cr
+++ src/stackcoin/const.cr
@@ -1,3 +1,4 @@
 module StackCoin
+  DEBUG = ENV["DEBUG"]?
   EPOCH = Time.unix(1574467200)
 end
--- src/stackcoin/log.cr
+++ src/stackcoin/log.cr
@@ -2,6 +2,21 @@ require "log"
 
 require "kemal"
 
+module StackCoin
+  Log = ::Log.for("stackcoin")
+end
+
+Log.setup do |c|
+  backend = Log::IOBackend.new
+
+  c.bind "*", :info, backend
+
+  if StackCoin::DEBUG
+    c.bind "stackcoin.*", :debug, backend
+    StackCoin::Log.debug { "Debug logging enabled" }
+  end
+end
+
 class CrystalLogHandler < Kemal::BaseLogHandler
   def initialize(@log = ::Log.for("kemal"))
   end
@@ -26,7 +41,3 @@ class CrystalLogHandler < Kemal::BaseLogHandler
 end
 
 Kemal.config.logger = CrystalLogHandler.new
-
-module StackCoin
-  Log = ::Log.for("stackcoin")
-end
