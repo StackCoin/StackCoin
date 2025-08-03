c5f482d4ea09544400d6e73833d828efaea5656e
Jack Arthur Harrhy <me@jackharrhy.com>
2025-08-03 16:06:08 -0230

    container'd

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,3 +17,30 @@ config :nostrum,
 if System.get_env("PHX_SERVER") do
   config :stackcoin, StackCoinWeb.Endpoint, server: true
 end
+
+if config_env() == :prod do
+  # The secret key base is used to sign/encrypt cookies and other secrets.
+  # A default value is used in config/dev.exs and config/test.exs but you
+  # want to use a different value for prod and you most likely don't want
+  # to check this value into version control, so we use an environment
+  # variable instead.
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  host = System.get_env("PHX_HOST") || "stackcoin.world"
+  port = String.to_integer(System.get_env("PORT") || "80")
+
+  config :stackcoin, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
+  config :stackcoin, StackCoinWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      ip: {0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base
+end
