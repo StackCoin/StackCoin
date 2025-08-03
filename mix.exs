c8d53506e8d9f80bda5c506af9db8f3d99c87b6a
Jack Arthur Harrhy <me@jackharrhy.com>
2025-08-03 13:58:35 -0230

    phoenix web server

--- mix.exs
+++ mix.exs
@@ -31,7 +31,28 @@ defmodule StackCoin.MixProject do
       {:dotenvy, "~> 1.1.0"},
       {:vega_lite, "~> 0.1.11"},
       {:vega_lite_convert, "~> 1.0"},
-      {:mock, "~> 0.3.0", only: :test}
+      {:mock, "~> 0.3.0", only: :test},
+      {:phoenix, "~> 1.7.21"},
+      {:phoenix_ecto, "~> 4.5"},
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 1.0"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:heroicons,
+       github: "tailwindlabs/heroicons",
+       tag: "v2.1.1",
+       sparse: "optimized",
+       app: false,
+       compile: false,
+       depth: 1},
+      {:telemetry_metrics, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"},
+      {:jason, "~> 1.2"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:plug_cowboy, "~> 2.7"}
     ]
   end
 end
