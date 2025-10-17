bacbbb827e24631145f5454f88630f80e355b30b
Jack Arthur Harrhy <me@jackharrhy.com>
2025-10-16 23:56:14 -0230

    give homepage some love

--- lib/stackcoin_web/controllers/page_controller.ex
+++ lib/stackcoin_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule StackCoinWeb.PageController do
   use StackCoinWeb, :controller
 
+  alias StackCoin.Core.Bank
+
   def home(conn, _params) do
-    render(conn, :home)
+    {:ok, %{transactions: transactions}} = Bank.search_transactions()
+
+    render(conn, :home, transactions: transactions)
   end
 end
