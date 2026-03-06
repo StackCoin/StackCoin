053874d8fe6a10e254c9610c3e17d04319b5087d
Jack Arthur Harrhy <me@jackharrhy.com>
2026-03-06 15:10:39 -0330

    feat: reject WebSocket joins when too many events are missed

--- lib/stackcoin_web/channels/bot_channel.ex
+++ lib/stackcoin_web/channels/bot_channel.ex
@@ -3,6 +3,8 @@ defmodule StackCoinWeb.BotChannel do
 
   alias StackCoin.Core.Event
 
+  @replay_limit 100
+
   @impl true
   def join("user:" <> user_id_str, payload, socket) do
     user_id =
@@ -12,11 +14,23 @@ defmodule StackCoinWeb.BotChannel do
       end
 
     if user_id == socket.assigns.user.id do
-      Phoenix.PubSub.subscribe(StackCoin.PubSub, "user:#{user_id}")
-
       last_event_id = Map.get(payload, "last_event_id", 0)
-      send(self(), {:replay_events, last_event_id})
-      {:ok, socket}
+      missed_count = Event.count_events_since(user_id, last_event_id)
+
+      if missed_count > @replay_limit do
+        {:error,
+         %{
+           reason: "too_many_missed_events",
+           missed_count: missed_count,
+           replay_limit: @replay_limit,
+           message:
+             "Use GET /api/events?since_id=#{last_event_id} to catch up, then reconnect with a recent last_event_id"
+         }}
+      else
+        Phoenix.PubSub.subscribe(StackCoin.PubSub, "user:#{user_id}")
+        send(self(), {:replay_events, last_event_id})
+        {:ok, socket}
+      end
     else
       {:error, %{reason: "unauthorized"}}
     end
